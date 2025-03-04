2d260cafaabad2fa50a844d83acbfae65bce179d
Rajesh Goyal <rajesh.goyal@qoala.id>
2025-03-04 14:19:50 +0530

    app changes to test argocd sync

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from Flask running in a container! V3 - helm chart"
+    return "Hello from Flask running in a container! V3 - helm chart & argocd"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3000)
