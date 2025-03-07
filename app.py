7c13333e606e8e057becf0dde996168b6e38b29a
Rajesh Goyal <rajesh.goyal@qoala.id>
2025-03-07 09:15:35 +0530

    test complete flow

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from Flask running in a container! V4 - helm chart & argocd"
+    return "Hello from Flask running in a container! V5 - helm chart & argocd"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3000)
