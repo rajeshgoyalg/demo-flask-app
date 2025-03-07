c9c61699aa5cb6bcfa6d26d6feeb90993ac4d422
Rajesh Goyal <rajesh.goyal@qoala.id>
2025-03-07 19:14:07 +0530

    CD updated to deploy on ECS

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from Flask running in a container! V5 - helm chart & argocd"
+    return "Hello from Flask running in a container! V5 - ECS CI / CD test"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3000)
